b012a17a415f5f748d38952f78026ee26ad5246b
Natalia Palej <palejnatalia@gmail.com>
2023-03-14 23:37:38 +0000

    Started on lab3 wk6 Lists

---
+++ .Semester2/Lab3_Lists/src/week6/ListSetPractice.java
@@ -0,0 +1,33 @@
+package week6;
+
+public class ListSetPractice {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void factoryMethods() {
+		
+	}
+	
+	public static void listOfNames() {
+		
+	}
+	
+	public static void stacks() {
+		
+	}
+	
+	public static void linkedLists() {
+		
+	}
+	
+	public static void treesets() {
+		
+	}
+	
+	public static void hashSet() {
+		
+	}
+
+}
