3c7db9ae38662c232bb9ee19aa2bbc0adb152d60
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 14:37:24 +0000

    Added methods a()b()c() printout for testing purpose

--- .Semester2/Lab4_ExceptionsPractice/src/week7/MyException.java
+++ .Semester2/Lab4_ExceptionsPractice/src/week7/MyException.java
@@ -5,17 +5,19 @@ class MyException extends Exception{
 	public static void main(String[] args) {
 		try {
 			a();
-		} catch (MyException e) {
+		} catch (Exception e) {
 			System.out.println("a(): " + e);
 		}
 	}
 	
-	public static void a() throws MyException {
+	public static void a() {
+		System.out.println("a()");
 		b();
 	}
 	
 	public static void b() {
 		try {
+			System.out.println("b()");
 			c();
 		} catch (MyException e) {
 			System.out.println("b(): " + e);
@@ -23,6 +25,7 @@ class MyException extends Exception{
 	}
 	
 	public static void c() throws MyException {
+		System.out.println("c()");
 		System.out.println("An I/O exception occurred");
 	}
 }
