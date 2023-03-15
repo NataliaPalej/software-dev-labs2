834ec6fa630b3bba1f31d4140f86969a6246a956
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 15:56:20 +0000

    Completed Ex1 mapTreeMap()

--- .Semester2/Lab5_Maps/src/week8/CollectionsPractice.java
+++ .Semester2/Lab5_Maps/src/week8/CollectionsPractice.java
@@ -1,12 +1,50 @@
 package week8;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CollectionsPractice {
 	
 	public static void main(String[] args) {
-		
+		mapHashMap();
 	}
 	
 	public static void mapHashMap() {
+		// TreeMap sorts the key:values
+		Map<String, Integer> map = new TreeMap<>();
+		map.put("Ford", 2019);
+		map.put("Audi", 2015);
+		map.put("Polo", 2023);
+		map.put("Beetle", 2012);
+		System.out.println(map);
+		System.out.println("containsKey \"Ford\": " + map.containsKey("Ford"));
+		System.out.println("containsValue \"2023\": " + map.containsValue(2023));
+		map.remove("Polo");
+		System.out.println(map);
+		map.replace("Audi", 2016);
+		System.out.println(map);
+		
+		int count = 0;
+		int keySize = map.keySet().size();
+		for(String key : map.keySet()) {
+			System.out.print(key);
+			if (count < keySize - 1) {
+				System.out.print(", ");
+			}
+			count++;
+		}
+		
+		System.out.println();
+		count=0;
+		int valSize = map.values().size();
+		for(int value : map.values()) {
+			System.out.print(value);
+			if (count < valSize - 1) {
+				System.out.print(", ");
+			}
+			count++;
+		}
+		
 		
 	}
 	
