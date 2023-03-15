ebfa096fcc5beab4062bd1a48285be38a78aae13
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 15:35:05 +0000

    Implemented method signature for wk8lab based on UML provided

---
+++ .Semester2/Lab5_Maps/src/week8/CollectionsPractice.java
@@ -0,0 +1,29 @@
+package week8;
+
+public class CollectionsPractice {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void mapHashMap() {
+		
+	}
+	
+	public static void mapTreeMap() {
+		
+	}
+	
+	public static void exceptions1() {
+		
+	}
+	
+	public static void exceptions2() {
+		
+	}
+	
+	public static void listOfFruits() {
+		
+	}
+
+}
