d69b2666f5fb5eb599a523bdf7f86efb50b564cf
Natalia Palej <palejnatalia@gmail.com>
2023-04-12 16:21:37 +0100

    Q3 completed, continuation of map() from wk9

--- .Semester2/Lab10_Practice/src/week10/code/CodePractice.java
+++ .Semester2/Lab10_Practice/src/week10/code/CodePractice.java
@@ -1,6 +1,8 @@
 package week10.code;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CodePractice {
 
@@ -13,7 +15,10 @@ public class CodePractice {
 		//}
 		
 		// Q2
-		generics();
+		//generics();
+		
+		// Q3
+		map();
 	}
 	
 	public static void generics() {
@@ -29,7 +34,31 @@ public class CodePractice {
 	}
 	
 	public static void map() {
+		// TreeMap or LinkedHashMap sorts automatically
+		Map<String, Integer> map = new TreeMap<>();
+		map.put("Ford", 2019);
+		map.put("Audi", 2015);
+		map.put("Polo", 2023);
+		map.put("Beetle", 2012);
+		System.out.println(map);
+		System.out.println("Contains \"Ford\": " + map.containsKey("Ford"));
+		System.out.println("Contains \"2023\": " + map.containsValue(2023));
+		map.remove("Polo");
+		System.out.println(map);
+		map.replace("Audi", 2016);
+		System.out.println(map);
 		
+		System.out.println();
+		System.out.println("Output keys:");
+		for (String key : map.keySet()) {
+			System.out.println(key);
+		}
+		
+		System.out.println();
+		System.out.println("Output values: ");
+		for (int value : map.values()) {
+			System.out.println(value);
+		}
 	}
 	
 	public static void listOfAnimals() {
